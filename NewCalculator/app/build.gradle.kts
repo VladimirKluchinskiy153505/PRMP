9a50630f068cc49a1b7d3dd05e7cd1b9fc61174b
VladimirKluchinskiy153505 <vova.klyuch@gmail.com>
2024-02-12 19:10:13 +0300

    Splash screen was added

--- NewCalculator/app/build.gradle.kts
+++ NewCalculator/app/build.gradle.kts
@@ -5,12 +5,12 @@ plugins {
 
 android {
     namespace = "com.example.newcalculator"
-    compileSdk = 34
+    compileSdk = 29
 
     defaultConfig {
         applicationId = "com.example.newcalculator"
         minSdk = 29
-        targetSdk = 34
+        targetSdk = 29
         versionCode = 1
         versionName = "1.0"
 
@@ -42,10 +42,10 @@ dependencies {
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation(files("libs\\MathParser.org-mXparser.jar"))
+    implementation("androidx.core:core-splashscreen:1.0.0")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-
 //    implementation(files("libs\\MathParser.org-mXparser-v.4.4.0-jdk8.jar"))
     //implementation("org.mariuszgromada.math:MathParser.org-mXparser:5.2.1")
 }
